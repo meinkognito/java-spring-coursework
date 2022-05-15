a70040c30d0bf00d8e37b98cb02d0261f7cea4b0
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-05-15 20:57:49 +0300

    Create User entity, repository and rest-controller

---
+++ maven/src/main/java/coursework/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package coursework.repositories;
+
+import coursework.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findUserByUsername(String username);
+}
